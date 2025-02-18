8c3672aace915868e3388d2722ade71b20974f85
Shams Ali <shamstali1@gmail.com>
2025-02-18 10:42:05 +0000

    Provide a list of comments associated with an article in the indv article page

--- components/ArticlesPage.jsx
+++ components/ArticlesPage.jsx
@@ -2,21 +2,26 @@ import { getArticles } from "../functions/axios.api";
 import { ArticlesNavbar } from "./sub-components/ArticlesNavbar";
 import { ArticleTile } from "./sub-components/ArticleTile";
 import { useEffect, useState } from "react";
+import { Preloader } from "./sub-components/Preloader";
 
 export const ArticlesPage = () => {
+  const [articles, setArticles] = useState(null);
 
-    const [articles, setArticles] = useState([])
+  useEffect(() => {
+    getArticles().then((articleData) => {
+      setArticles(articleData);
+    });
+  }, []);
 
-    useEffect(()=>{
-        getArticles().then((articleData)=>{
-            setArticles(articleData)
-        })
-    }, [])
+  if (articles === null) {
+    return <Preloader />;
+  }
 
   return (
     <div className="articles-page-container">
       <ArticlesNavbar />
-      <ArticleTile articles={articles}/>
+      <ArticleTile articles={articles} />
+      <p className="p-test">Scroll Box</p>
     </div>
   );
 };
--- components/IndvArticlePage.jsx
+++ components/IndvArticlePage.jsx
@@ -10,6 +10,7 @@ import {
   Paper,
 } from "@mui/material";
 import { Preloader } from "./sub-components/Preloader";
+import { CommentsList } from "./sub-components/CommentsList";
 
 export const IndvArticlePage = () => {
   const { article_id } = useParams();
@@ -27,29 +28,32 @@ export const IndvArticlePage = () => {
   }
 
   return (
-    <Container maxWidth="md" sx={{ mt: 4 }}>
-      <Paper elevation={3} sx={{ p: 3, borderRadius: 3 }}>
-        <Card sx={{ borderRadius: 3, boxShadow: 3 }}>
-          <CardMedia
-            component="img"
-            height="300"
-            image={selectArticle.article_img_url}
-            alt={selectArticle.title}
-            sx={{ borderTopLeftRadius: 12, borderTopRightRadius: 12 }}
-          />
-          <CardContent>
-            <Typography variant="h4" gutterBottom>
-              {selectArticle.title}
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary">
-              By <strong>{selectArticle.author}</strong>
-            </Typography>
-            <Typography variant="body1" sx={{ mt: 2 }}>
-              {selectArticle.body}
-            </Typography>
-          </CardContent>
-        </Card>
-      </Paper>
-    </Container>
+    <div className="indv-article-container">
+      <Container maxWidth="md" sx={{ mt: 4 }}>
+        <Paper elevation={3} sx={{ p: 3, borderRadius: 3 }}>
+          <Card sx={{ borderRadius: 3, boxShadow: 3 }}>
+            <CardMedia
+              component="img"
+              height="300"
+              image={selectArticle.article_img_url}
+              alt={selectArticle.title}
+              sx={{ borderTopLeftRadius: 12, borderTopRightRadius: 12 }}
+            />
+            <CardContent>
+              <Typography variant="h4" gutterBottom>
+                {selectArticle.title}
+              </Typography>
+              <Typography variant="subtitle1" color="text.secondary">
+                By <strong>{selectArticle.author}</strong>
+              </Typography>
+              <Typography variant="body1" sx={{ mt: 2 }}>
+                {selectArticle.body}
+              </Typography>
+            </CardContent>
+          </Card>
+        <CommentsList />
+        </Paper>
+      </Container>
+    </div>
   );
 };
